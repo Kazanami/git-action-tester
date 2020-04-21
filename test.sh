7453377531f0fe00b736e183058ab6ca097c1937
Kazanami <6745680+Kazanami@users.noreply.github.com>
2020-04-21 14:06:01 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+git remote set-url origin "https://Kazanami:${{secrets.GITHUB_TOKEN}}@github.com/Kazanami/git-action-tester"
+git config user.name "Github Action Runner"
+git config user.email "github-action-runner@example.com"
+git commit -m "recursive added" --allow-empty
+git push origin master
 git add .;
 echo "Git added Exit: ${?}"
 exit 0
